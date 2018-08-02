347372fe1e14119c64f1cf264fd02ad1bd97b427
ToddWebDev <webdevtodd@gmail.com>
2018-08-01 19:35:55 -0500

    Add Firebase for Authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, FormInput } from 'react-native-elements';
 import InnerSection from './InnerSection';
+import firebase from 'firebase';
 
 export default class Login extends React.Component {
+  componentDidMount(){
+    const config = {
+      apiKey: "AIzaSyDtBC8PhXkiWlrhZKGlXENmREhEjVmdX4I",
+      authDomain: "doorman-auth.firebaseapp.com",
+      databaseURL: "https://doorman-auth.firebaseio.com",
+      projectId: "doorman-auth",
+      storageBucket: "doorman-auth.appspot.com",
+      messagingSenderId: "484408441043"
+    };
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <View>
